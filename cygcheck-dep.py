9f98b96f3a55723cc49be54f631f4a58530b9924
Ken Brown <kbrown@cornell.edu>
2016-10-10 12:27:45 -0400

    Add some error checking

--- cygcheck-dep.py
+++ cygcheck-dep.py
@@ -56,21 +56,29 @@ def get_installed_pkgs():
             sys.exit(1)
         return match.group(1).split()
 
-# Reverse a directed graph.
-def reverse(g):
+# Reverse a dependency graph g with installed packages I
+def reverse(g, I):
     h = defaultdict(list)
-    for v, e in g.items():
-        for w in e:
-            h[w].append(v)
+    warnings = defaultdict(list)
+    for p, req in g.items():
+        for q in req:
+            h[q].append(p)
+            if not q in I:
+                warnings[p].append(q)
+    if warnings:
+        for p, req in warnings.items():
+            print("Warning: %s requires the following uninstalled package(s):" % p)
+            print(req)
+        print("Warning: Any results that follow are unreliable.")
     return h
 
 def main():
     parser = argparse.ArgumentParser(description='Find dependency information for Cygwin installation')
     parser.add_argument('-i', '--inifile', action='store', help='path to setup.ini', required=False, metavar='FILE')
-    parser.add_argument('package', help='package name', metavar='PACKAGE')
+    parser.add_argument('package', help='package name', metavar='PACKAGE', nargs='?')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-r', '--requires', action='store_true', help='print dependencies')
-    group.add_argument('-R', '--recursively-requires', action='store_true', dest='Requires', help='print recursive dependencies')
+    group.add_argument('-r', '--requires', action='store_true', help='print dependencies of PACKAGE')
+    group.add_argument('-R', '--recursively-requires', action='store_true', dest='Requires', help='print recursive dependencies of PACKAGE')
     group.add_argument('-n', '--needs', action='store_true', help='print packages that require PACKAGE')
     group.add_argument('-N', '--recursively-needs', action='store_true', dest='Needs', help='print packages that recursively require PACKAGE')
     args = parser.parse_args()
@@ -86,14 +94,23 @@ def main():
 
     inst_pkgs_graph = {p: all_pkgs_graph[p] for p in inst}
 
+    rev_graph = reverse(inst_pkgs_graph, inst)
+
+    if args.requires or args.Requires or args.needs or args.Needs:
+        if not args.package:
+            print("PACKAGE must be specified")
+            sys.exit(1)
+        if args.package not in inst:
+            print("%s is not installed." % args.package)
+            sys.exit(1)
     if args.requires:
         print(sorted(inst_pkgs_graph[args.package]))
     elif args.Requires:
         print(sorted(tarjan.tc.tc(inst_pkgs_graph)[args.package]))
     elif args.needs:
-        print(sorted(reverse(inst_pkgs_graph)[args.package]))
+        print(sorted(rev_graph[args.package]))
     elif args.Needs:
-        print(sorted(tarjan.tc.tc(reverse(inst_pkgs_graph))[args.package]))
+        print(sorted(tarjan.tc.tc(rev_graph)[args.package]))
 
 
 if __name__ == '__main__':
