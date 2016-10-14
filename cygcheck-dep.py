8f01c8dea1b5e5015d5160c299c76a8ccaa40d25
Ken Brown <kbrown@cornell.edu>
2016-10-14 14:52:44 -0400

    Implement -a,--all-packages option

--- cygcheck-dep.py
+++ cygcheck-dep.py
@@ -39,6 +39,7 @@ def parse_setup_ini(inifile):
             if match:
                 # New package
                 name = match.group(1)
+                g[name] = []
                 continue
 
             if(re.match(r'^category:.*\bBase\b', line)):
@@ -80,14 +81,16 @@ def reverse(g, I):
 
 def main():
     parser = argparse.ArgumentParser(description='Find dependency information for Cygwin installation')
-    parser.add_argument('-i', '--inifile', action='store', help='path to setup.ini', required=False, metavar='FILE')
+    parser.add_argument('-p', '--inifile', action='store', help='path to setup.ini', required=False, metavar='FILE')
+    parser.add_argument('-a', '--all-packages', action='store_true', dest='all', help='report on all packages, not just those installed')
     parser.add_argument('package', help='package name', metavar='PACKAGE', nargs='?')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-r', '--requires', action='store_true', help='print dependencies of PACKAGE')
-    group.add_argument('-R', '--recursively-requires', action='store_true', dest='Requires', help='print recursive dependencies of PACKAGE')
-    group.add_argument('-n', '--needs', action='store_true', help='print packages that require PACKAGE')
-    group.add_argument('-N', '--recursively-needs', action='store_true', dest='Needs', help='print packages that recursively require PACKAGE')
-    group.add_argument('-l', '--leaves', action='store_true', help='print leaves of dependency graph')
+    group.add_argument('-r', '--requires', action='store_true', help='show dependencies of PACKAGE')
+    group.add_argument('-R', '--recursively-requires', action='store_true', dest='Requires', help='show recursive dependencies of PACKAGE')
+    group.add_argument('-n', '--needs', action='store_true', help='show packages that require PACKAGE')
+    group.add_argument('-N', '--recursively-needs', action='store_true', dest='Needs', help='show packages that recursively require PACKAGE')
+    group.add_argument('-l', '--leaves', action='store_true', help='show leaves of dependency graph')
+    # group.add_argument('-i', '--islands', action='store_true', help='show strongly connected components not required by any others')
     args = parser.parse_args()
 
     inifile = get_setup_ini(args)
@@ -97,31 +100,37 @@ def main():
 
     all_pkgs_graph = parse_setup_ini(inifile)
 
-    inst = get_installed_pkgs()
-    inst_plus_base = inst[:]    # Copy by slicing.
-    inst_plus_base.append('BASE')
+    # Create working dependency graph g, which always includes 'BASE'.
+    if not args.all:
+        inst = get_installed_pkgs()
+        inst_plus_base = inst[:]    # Copy by slicing.
+        inst_plus_base.append('BASE')
+        g = {p : all_pkgs_graph[p] for p in inst_plus_base}
+    else:
+        g = all_pkgs_graph
+        inst_plus_base = list(g.keys())
+        inst = inst_plus_base[:]
+        inst.remove('BASE')
 
-    inst_pkgs_graph = {p : all_pkgs_graph[p] for p in inst_plus_base}
-
-    rev_graph = reverse(inst_pkgs_graph, inst_plus_base)
+    rev_g = reverse(g, inst_plus_base)
 
     if args.requires or args.Requires or args.needs or args.Needs:
         if not args.package:
             print("PACKAGE must be specified")
             sys.exit(1)
         if args.package not in inst:
-            print("%s is not installed." % args.package)
+            print("%s is not installed or not known." % args.package)
             sys.exit(1)
     if args.requires:
-        print(sorted(inst_pkgs_graph[args.package]))
+        print(sorted(g[args.package]))
     elif args.Requires:
-        print(sorted(tarjan.tc.tc(inst_pkgs_graph)[args.package]))
+        print(sorted(tarjan.tc.tc(g)[args.package]))
     elif args.needs:
-        print(sorted(rev_graph[args.package]))
+        print(sorted(rev_g[args.package]))
     elif args.Needs:
-        print(sorted(tarjan.tc.tc(rev_graph)[args.package]))
+        print(sorted(tarjan.tc.tc(rev_g)[args.package]))
     elif args.leaves:
-        print(sorted([p for p in inst if not rev_graph[p]]))
+        print(sorted([p for p in inst if not rev_g[p]]))
 
 
 if __name__ == '__main__':
